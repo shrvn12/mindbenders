c44597bfdf43ea3d6373a45557d9561e8f9a8d11
shrvn12 <shravan1208@gmail.com>
2024-04-28 14:44:26 +0530

    minor fix

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-const connection = mongoose.connect('mongodb://127.0.0.1:27017/mindbenders');
+const connection = mongoose.connect(process.env.MONGO_URL);
 
 module.exports = {
     connection
