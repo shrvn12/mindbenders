821c52da32e44a918e7cb47642e29bf753352d3a
shrvn12 <shravan1208@gmail.com>
2024-04-28 15:12:48 +0530

    refactor: changes in routing

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,159 @@
-var express = require('express');
-const jwt = require('jsonwebtoken');
+var express = require("express");
+const jwt = require("jsonwebtoken");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const token = req.cookies.token
+router.get("/", function (req, res, next) {
+  const token = req.cookies.token;
 
   if (!token) {
-    return res.render('index', { title: 'Express', auth: "Login", auth_url: '/login' });
+    return res.render("index", {
+      title: "Express",
+      auth: "Login",
+      auth_url: "/login",
+    });
   }
 
   jwt.verify(token, process.env.USER_TOKEN_KEY, (err, decoded) => {
     if (err) {
       console.log(err);
-      return res.render('index', { title: 'Express' });
+      return res.render("index", { title: "Express" });
     } else {
       console.log(decoded);
       if (decoded && decoded.name) {
-        res.render('index', { title: 'Express', auth: "Logout", auth_url: '/logout', user: decoded });
-      }
-      else {
-        res.render('index', { title: 'Express', auth: "Logout", auth_url: '/logout'});
+        res.render("index", {
+          title: "Express",
+          auth: "Logout",
+          auth_url: "/logout",
+          user: decoded,
+        });
+      } else {
+        res.render("index", {
+          title: "Express",
+          auth: "Logout",
+          auth_url: "/logout",
+        });
       }
     }
   });
-
 });
 
-router.get('/login', function(req, res) {
-  res.render('login');
-})
+router.get("/login", function (req, res) {
+  res.render("login");
+});
 
-router.get('/logout', function(req, res) {
-  res.clearCookie('token');
-  return res.render('index', { title: 'Express', auth: "Login", auth_url: '/login' });
-})
+router.get("/logout", function (req, res) {
+  res.clearCookie("token");
+  return res.render("index", {
+    title: "Express",
+    auth: "Login",
+    auth_url: "/login",
+  });
+});
+
+router.get("/course", (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.render("index", {
+      title: "Express",
+      auth: "Login",
+      auth_url: "/login",
+    });
+  }
+
+  jwt.verify(token, process.env.USER_TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.render("index", { title: "Express" });
+    } else {
+      console.log(decoded);
+      if (decoded && decoded.name) {
+        res.render("course", {
+          title: "Express",
+          auth: "Logout",
+          auth_url: "/logout",
+          user: decoded,
+        });
+      } else {
+        res.render("course", {
+          title: "Express",
+          auth: "Logout",
+          auth_url: "/logout",
+        });
+      }
+    }
+  });
+});
+
+router.get("/blog", (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.render("index", {
+      title: "Express",
+      auth: "Login",
+      auth_url: "/login",
+    });
+  }
+
+  jwt.verify(token, process.env.USER_TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.render("index", { title: "Express" });
+    } else {
+      console.log(decoded);
+      if (decoded && decoded.name) {
+        res.render("blog", {
+          title: "Express",
+          auth: "Logout",
+          auth_url: "/logout",
+          user: decoded,
+        });
+      } else {
+        res.render("blog", {
+          title: "Express",
+          auth: "Logout",
+          auth_url: "/logout",
+        });
+      }
+    }
+  });
+});
+
+router.get("/contact", (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.render("index", {
+      title: "Express",
+      auth: "Login",
+      auth_url: "/login",
+    });
+  }
+
+  jwt.verify(token, process.env.USER_TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.render("index", { title: "Express" });
+    } else {
+      console.log(decoded);
+      if (decoded && decoded.name) {
+        res.render("contact", {
+          title: "Express",
+          auth: "Logout",
+          auth_url: "/logout",
+          user: decoded,
+        });
+      } else {
+        res.render("contact", {
+          title: "Express",
+          auth: "Logout",
+          auth_url: "/logout",
+        });
+      }
+    }
+  });
+});
 
 module.exports = router;
